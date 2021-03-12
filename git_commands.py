07f15e02662a98977f7f00347d56684f0ca4bcc2
azamat <l0808las@gmail.com>
2021-03-12 20:00:45 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,10 @@
+git init - .git
+git remote add name url
+git pull origin master
+get status
+git add .
+git commit -m "comment"
+git branch name branch
+git checkout name branch
+git push origin master
+git reset filename
